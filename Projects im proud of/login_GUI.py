1fc0848dafc6d652ebed290dd14d431f96036267
DanieliusDev <51119867+DanieliusDev@users.noreply.github.com>
2021-02-04 20:56:32 +0200

    New variable

--- Projects im proud of/login_GUI.py
+++ Projects im proud of/login_GUI.py
@@ -6,7 +6,7 @@ import tkinter as tk
 
 # todo so far everything looks good now time to implement this into my
 #  main terminal :)
-
+a = 15
 def username(txt_username):
     if txt_username == "1":
         return True
