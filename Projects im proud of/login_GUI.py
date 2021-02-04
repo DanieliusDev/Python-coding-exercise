1a53f7dc664dc7091646ea3b7eaec9a94d62df14
DanieliusDev <51119867+DanieliusDev@users.noreply.github.com>
2021-02-05 00:09:07 +0200

    where this goes ?

--- Projects im proud of/login_GUI.py
+++ Projects im proud of/login_GUI.py
@@ -6,7 +6,7 @@ import tkinter as tk
 
 # todo so far everything looks good now time to implement this into my
 #  main terminal :)
-
+#  Update to see where this goes
 
 def username(txt_username):
     if txt_username == "1":
